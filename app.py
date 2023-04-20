8b375c4b5abb09334890759f37379c4d7388a782
iyer51 <54751683+iyer51@users.noreply.github.com>
2023-04-20 11:56:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import serial
+import eventlet
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'templates')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-ser = serial.Serial('/dev/tty1', 9600)
+ser = serial.Serial('/dev/serial0', 9600)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @socketio.on('connect')
 def test_connect():
     print('Client connected')
-    send_board_state()
 
 @socketio.on('disconnect')
 def test_disconnect():
@@ -20,32 +24,25 @@ def test_disconnect():
 @socketio.on('piece_moved')
 def piece_moved(move):
     print(f'Piece moved from {move["from"]} to {move["to"]}')
-    send_board_state()
-    
-@app.route('/move', methods=['POST'])
-def move():
-    move_data = request.json
-    from_coord = (move_data['pick'] % 5 + 1, move_data['pick'] // 5 + 1)
-    to_coord = (move_data['place'] % 5 + 1, move_data['place'] // 5 + 1)
-    move_str = f'Piece moved from {from_coord} to {to_coord}'.encode('utf-8')
-    ser.write(move_str)
-    return 'Move received'
+
+    # Parse the move data
+    fromIndex = move['from']
+    toIndex = move['to']
+    fromRow = int(fromIndex / 5) + 1
+    fromCol = fromIndex % 5 + 1
+    toRow = int(toIndex / 5) + 1
+    toCol = toIndex % 5 + 1
+
+    # Construct the message to send to the STM
+    message = f'{fromRow},{fromCol},{toRow},{toCol}\n'
+    messageBytes = message.encode('utf-8')
+
+    # Send the message over UART
+    ser.write(messageBytes)
 
 @socketio.on('reset_board')
 def reset_board():
     print('Board reset')
-    send_board_state()
-
-def send_board_state():
-    board_state = []
-    for i in range(25):
-        board_state.append(int(ser.readline().decode('ascii').strip()))
-    print(board_state)
-    socketio.emit('update_board', board_state)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app)
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
