cd438f4602573cd9e3e62c1f0e0c34988f5fa328
iyer51 <54751683+iyer51@users.noreply.github.com>
2023-04-21 04:16:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import serial
 import eventlet
+import time
 
-app = Flask(__name__, template_folder = 'templates')
+app = Flask(__name__, template_folder = 'templates', static_folder = 'static')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-ser = serial.Serial('/dev/serial0', 9600)
+uart_port='/dev/ttyUSB0'
+baudrate=9600
 
+ser = serial.Serial(uart_port,baudrate,timeout=1)
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return ""
+    
 @socketio.on('connect')
-def test_connect():
+def test_connect(data):
     print('Client connected')
+    
 
 @socketio.on('disconnect')
 def test_disconnect():
@@ -24,25 +32,35 @@ def test_disconnect():
 @socketio.on('piece_moved')
 def piece_moved(move):
     print(f'Piece moved from {move["from"]} to {move["to"]}')
-
-    # Parse the move data
-    fromIndex = move['from']
-    toIndex = move['to']
-    fromRow = int(fromIndex / 5) + 1
-    fromCol = fromIndex % 5 + 1
-    toRow = int(toIndex / 5) + 1
-    toCol = toIndex % 5 + 1
-
-    # Construct the message to send to the STM
-    message = f'{fromRow},{fromCol},{toRow},{toCol}\n'
-    messageBytes = message.encode('utf-8')
-
-    # Send the message over UART
-    ser.write(messageBytes)
+    send_board_state()
+    
+    
 
 @socketio.on('reset_board')
 def reset_board():
     print('Board reset')
+    send_board_state()
+
+
+def send_board_state():
+    print('Sending board state')
+    board_state = []
+    for i in range(25):
+        board_state.append(int(ser.readline().decode('utf-8').strip()))
+    print(board_state)
+    socketio.emit('update', board_state)
+    
+    
+@app.route('/move', methods=['POST'])
+def move():
+    move_data = request.json
+    from_coord = (move_data['pick'] % 5 + 1, move_data['pick'] // 5 + 1)
+    to_coord = (move_data['place'] % 5 + 1, move_data['place'] // 5 + 1)
+    move_str = f'Pick from {from_coord} place {to_coord}'.encode('utf-8')
+    ser.write(move_str)
+    
+    return 'Move received'
+
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
+    eventlet.wsgi.server(eventlet.listen(('128.46.96.239', 5000)), app)
